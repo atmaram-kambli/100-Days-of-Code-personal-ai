f01cca109aa2596a6bf147838f9c61b6cfef8960
Atmaram Kamlbi <118362436+atmaram-kambli@users.noreply.github.com>
2024-01-01 15:49:20 +0530

    Create Day32.cpp

---
+++ Day32.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool canPair(vector<int> nums, int k) {
+        if(nums.size()%2 != 0) return false;
+        
+        int arr[k] = {0};
+        for(int i = 0; i < nums.size(); i++) {
+            arr[nums[i]%k]++;
+        }
+        if(arr[0]%2 != 0) return false;
+        
+        int start = 1, end = k - 1;
+        while(start <= end) {
+            if(arr[start++] != arr[end--]) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n, k;
+        cin >> n >> k;
+        vector<int> nums(n);
+        for (int i = 0; i < nums.size(); i++) cin >> nums[i];
+        Solution ob;
+        bool ans = ob.canPair(nums, k);
+        if (ans)
+            cout << "True\n";
+        else
+            cout << "False\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
